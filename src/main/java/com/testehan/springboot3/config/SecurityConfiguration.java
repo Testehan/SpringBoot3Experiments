2af2fc7acf53d4128af9c8f1e1994e15f5e6bcdc
DanTestehan <tdan89@yahoo.com>
2023-05-26 17:19:39 +0300

    SecurityConfiguration responsible for configuration of spring security

---
+++ src/main/java/com/testehan/springboot3/config/SecurityConfiguration.java
@@ -0,0 +1,39 @@
+package com.testehan.springboot3.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+
+    private final JWTAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()           // disable CSRF
+                .authorizeHttpRequests()
+                .requestMatchers("api/v1/auth/**")// to access these patterns a user can be NOT authenticated
+                    .permitAll()
+                .anyRequest()// all other patterns are need to be authenticated
+                    .authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return httpSecurity.build();
+    }
+}
---
+++ src/test/java/com/testehan/springboot3/SpringBoot3ExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testehan.springboot3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ExperimentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
